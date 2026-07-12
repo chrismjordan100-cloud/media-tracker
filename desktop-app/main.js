69a5c5f19dfd451a5456eac88639faa0c1fa7563
chrismjordan100-cloud <chrismjordan100@gmail.com>
2026-07-12 17:47:44 +0100

    Cache-bust desktop app URL so it always loads latest

--- desktop-app/main.js
+++ desktop-app/main.js
@@ -13,8 +13,8 @@ function createWindow() {
     }
   });
 
-  // Load the AWS hosted version
-  win.loadURL('http://media-tracker-app-eggnog-915238109618-eu-north-1-an.s3-website.eu-north-1.amazonaws.com');
+  // Load the AWS hosted version (cache-bust with timestamp)
+  win.loadURL('http://media-tracker-app-eggnog-915238109618-eu-north-1-an.s3-website.eu-north-1.amazonaws.com?v=' + Date.now());
 }
 
 app.whenReady().then(createWindow);
